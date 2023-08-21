fd294abf5e6e5147826cccb8bfbb21f01313197e
Vladislav Kolegov <v.kolegov@outlook.com>
2023-08-21 14:57:51 +0700

    src/middleware: support for per-page cache postfix

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,12 @@ const path = require("path");
 const {serialize, deserialize} = require("./serializer");
 const makeCache = require("./cache-builders");
 
+/**
+ * @typedef {object} PageToCache
+ * @property {string|RegExp} url
+ * @property {string | ((ctx: object) => string) | null} cacheKeyPostfix
+ */
+
 function cleanIfNewVersion(cache, version) {
   if (!version) {
     return;
@@ -35,19 +41,64 @@ module.exports = function cacheRenderer(moduleOptions) {
     return;
   }
 
+  /**
+   *
+   * @param {string} path
+   * @param {object} context
+   * @returns {boolean}
+   */
   function isCacheFriendly(path, context) {
     if (typeof (moduleOptions.isCacheable) === "function") {
       return moduleOptions.isCacheable(path, context);
     }
 
-    return !context.res.spa &&
-      moduleOptions.pages.some(pat =>
-        pat instanceof RegExp
-          ? pat.test(path)
-          : path.startsWith(pat)
-      );
+    if (context.res.spa) {
+      return false;
+    }
+
+    return getPageByPath(path) !== null;
   }
 
+  /**
+   *
+   * @param {string} path
+   * @returns {PageToCache|null}
+   */
+  function getPageByPath(path) {
+    /**
+     * @type {PageToCache[]|string[]}
+     */
+    const pages = moduleOptions.pages;
+
+
+    for (const page of pages) {
+
+      const url = (typeof page === "string" || page instanceof RegExp)
+        ? page
+        : page.url;
+
+      const regexpMatch = (url instanceof RegExp) && url.test(path);
+
+      if (regexpMatch) {
+        return page;
+      }
+
+      const stringMatch = (typeof url === "string") && path.startsWith(url);
+
+      if (stringMatch) {
+        return page;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   *
+   * @param {string} route
+   * @param {object} context
+   * @returns {string|boolean}
+   */
   function defaultCacheKeyBuilder(route, context) {
     if (!isCacheFriendly(route, context)) {
       return false;
@@ -65,17 +116,31 @@ module.exports = function cacheRenderer(moduleOptions) {
       }
     }
 
+    let cacheString;
+
     // /catalog/accessories/ -> catalog.accessories
-    let pageString = route
+    const routeString = route
       .split("/")
       .filter(char => char !== "")
       .join(".");
 
     if (moduleOptions.prefix) {
-      return `${moduleOptions.prefix}:page:${pageString}`;
+      cacheString = `${moduleOptions.prefix}:page:${routeString}`;
+    } else {
+      cacheString = `page:${routeString}`;
     }
 
-    return `page:${pageString}`;
+    const page = getPageByPath(route);
+
+    if (page && page.cacheKeyPostfix) {
+      if (typeof page.cacheKeyPostfix === "string") {
+        cacheString = `${cacheString}:${page.cacheKeyPostfix}`;
+      } else {
+        cacheString += ":" + page.cacheKeyPostfix(context);
+      }
+    }
+
+    return cacheString;
   }
 
   function cacheKeyBuilder(key, route, context) {
