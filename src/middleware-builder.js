72a065a30fd4e95d63aa7de83462e5310f45b56d
Vladislav Kolegov <dev@vkolegov.ru>
2023-09-29 18:28:40 +0700

    src/middleware-builder

---
+++ src/middleware-builder.js
@@ -0,0 +1,74 @@
+/**
+ *
+ * @param {PageToCache[]|string[]} pages
+ * @returns {(function(context: import('@nuxt/types').Context): Promise<void>)|*}
+ */
+export function makeSsrCacheMiddleware(pages) {
+
+  /**
+   *
+   * @param {string} routePath
+   * @returns {PageToCache|null}
+   */
+  function getPageByPath(routePath) {
+
+    for (const page of pages) {
+      const url =
+        typeof page === 'string' || page instanceof RegExp ? page : page.url;
+
+      const regexpMatch = url instanceof RegExp && url.test(routePath);
+
+      if (regexpMatch) {
+        return page;
+      }
+
+      const stringMatch = typeof url === 'string' && routePath.startsWith(url);
+
+      if (stringMatch) {
+        return page;
+      }
+    }
+
+    return null;
+  }
+
+
+  return async context => {
+
+    if (context.ssrContext?.skipCacheCheck) {
+      return;
+    }
+
+    const route = context.route.fullPath;
+
+    console.log(`[cache middleware] route ${route}`);
+
+    if (!process.server) {
+      return;
+    }
+
+
+    const page = getPageByPath(route);
+
+    if (!page) {
+      return;
+    }
+
+    console.log(`[cache middleware] ${route} is going to be cached`);
+
+    let cacheKeyPostfix = null;
+
+    if (typeof page.cacheKeyPostfix === 'string') {
+      cacheKeyPostfix = page.cacheKeyPostfix;
+    } else if (typeof page.cacheKeyPostfix === 'function') {
+      cacheKeyPostfix = page.cacheKeyPostfix(context);
+    }
+
+    const message = new Error('Process page cache');
+
+    message.code = 999;
+    message.cacheKeyPostfix = cacheKeyPostfix;
+
+    throw message;
+  };
+}
